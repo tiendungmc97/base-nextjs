963101a0c38edec3bccc873b30472cada87fce38
DungNT <tiendungmc97@gmail.com>
2024-09-01 22:10:01 +0700

    [base]: multi-language

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,31 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import createNextIntlPlugin from 'next-intl/plugin';
 
-export default nextConfig;
+const withNextIntl = createNextIntlPlugin();
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    serverActions: true,
+  },
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/home',
+        permanent: true,
+      },
+      {
+        source: '/en',
+        destination: '/en/home',
+        permanent: true,
+      },
+      {
+        source: '/ja',
+        destination: '/ja/home',
+        permanent: true,
+      },
+    ]
+  },
+};
+
+export default withNextIntl(nextConfig);
